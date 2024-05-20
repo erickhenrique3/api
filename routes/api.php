d2d46a5069942a24efb70d021a760b30c3c43f0f
erickhenrique3 <erick.2henriquehtp@gmail.com>
2024-05-19 21:31:19 -0300

    rotas agrupadas

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\MotoController;
 use App\Http\Controllers\SubtasksController;
 use App\Http\Controllers\TasksController;
 use App\Models\Subtarefas;
+use App\Models\Subtasks;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,23 +23,40 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('tasks')->controller(TasksController::class)->group(function () {
-    Route::post('/', 'create');
-    Route::get('/', 'index');
-    Route::get('/{tasks}', 'show');
-    Route::put('/{tasks}', 'update');
-    Route::patch('/{tasks}/due_date', 'patch');
-    Route::patch('/{tasks}/status', 'updateStatus');
-    Route::delete('/{tasks}', 'destroy');
-    Route::get('/filter/today', 'filterToday'); 
-    Route::get('/filter/overdue', 'filterOverdue');
+Route::prefix('tasks')->group(function () {
+    Route::controller(TasksController::class)->group(function () {
+        Route::post('/', 'create');
+        Route::get('/', 'index');
+        Route::get('/{tasks}', 'show');
+        Route::put('/{tasks}', 'update');
+        Route::patch('/{tasks}/due_date', 'patch');
+        Route::patch('/{tasks}/status', 'updateStatus');
+        Route::delete('/{tasks}', 'destroy');
+        Route::get('/filter/today', 'filterToday'); 
+        Route::get('/filter/overdue', 'filterOverdue');
+
+    
+        Route::prefix('subtasks')->group(function () {
+            Route::post('/', [SubtasksController::class, 'create']);
+            Route::get('/', [SubtasksController::class, 'index']);
+            Route::get('/{subtasks}', [SubtasksController::class, 'show']);
+            Route::put('/{id}', [SubtasksController::class, 'update']);
+            Route::patch('/{id}', [SubtasksController::class, 'updateStatus']);
+            Route::delete('/{subtasks}', [SubtasksController::class, 'destroy']);
+        });
+    });
 });
 
-Route::prefix('subtasks')->controller(SubtasksController::class)->group(function () {
-    Route::post('/', 'create');
-    Route::get('/', 'index');
-    Route::get('/{subtasks}', 'show');
-    Route::put('/{id}', 'update');
-    Route::patch('/{id}', 'updateStatus');
-    Route::delete('/{subtasks}', 'destroy');
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
