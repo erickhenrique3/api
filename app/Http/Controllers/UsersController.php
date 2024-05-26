bdd30415ed3d20f030082c05620cb961be985007
erickhenrique3 <erick.2henriquehtp@gmail.com>
2024-05-26 15:34:03 -0300

    token finalizado

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+        return response()->json($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(null, 204);
+    }
+}
+    /**
+     * Display a listing of the resource.
+     */
+  
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,15 @@
 use App\Http\Controllers\MotoController;
 use App\Http\Controllers\SubtasksController;
 use App\Http\Controllers\TasksController;
+use App\Http\Controllers\UsersController;
 use App\Models\Subtarefas;
 use App\Models\Subtasks;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +24,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('api')->group(function () {
+    // esse resource faz:
+    //Por exemplo, ao usar Route::resource('users', UserController::class), o Laravel irá gerar as seguintes rotas automaticamente:
+
+    // GET /users - Retorna todos os usuários.
+    // POST /users - Armazena um novo usuário.
+    // GET /users/{user} - Retorna os detalhes de um usuário específico.
+    // PUT/PATCH /users/{user} - Atualiza os detalhes de um usuário específico.
+    // DELETE /users/{user} - Exclui um usuário específico.
+    Route::resource('user', UsersController::class);
+});
+
+
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+
+    if (!$token = JWTAuth::attempt($credentials)) {
+        abort(401, 'unauthorized');
+    }
+    return  response()->json([
+        'data' => [
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => JWTAuth::factory()->getTTL() * 60
+        ]
+
+
+    ]);
 });
 
 Route::prefix('tasks')->group(function () {
@@ -32,10 +68,10 @@ Route::prefix('tasks')->group(function () {
         Route::patch('/{tasks}/due_date', 'patch');
         Route::patch('/{tasks}/status', 'updateStatus');
         Route::delete('/{tasks}', 'destroy');
-        Route::get('/filter/today', 'filterToday'); 
+        Route::get('/filter/today', 'filterToday');
         Route::get('/filter/overdue', 'filterOverdue');
 
-    
+
         Route::prefix('subtasks')->group(function () {
             Route::post('/', [SubtasksController::class, 'create']);
             Route::get('/', [SubtasksController::class, 'index']);
@@ -46,17 +82,3 @@ Route::prefix('tasks')->group(function () {
         });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
